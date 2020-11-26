ba9ac9b5bd30c4ad7e5b919c23de856b2fe03004
rpetersson <rpetersson@gmail.com>
2020-11-26 21:08:08 +0100

    adding script file

---
+++ script.js
@@ -0,0 +1,25 @@
+//question 1
+var firstVar
+//question 2
+var name  = "Robert Petersson"
+//question 3
+var numberValue = 9
+//question 4
+var division = 20 / 5
+//question 5
+var variable = "frog";
+typeof variable;
+//question 6
+var orderHasShipped = true
+//question 7
+if (orderHasShipped == true) {
+    consoleLog("true")
+}else {
+    consoleLog("false")
+}
+//question 8
+
+var i
+for(i = 0; i <= 9; i++) {
+    consoleLog(i)
+}
